6c8a33992a05e394dcaa44f0744eb869b19dc62b
XimYa <hj_lee_com@naver.com>
2023-11-14 19:06:15 +0900

    chore : initialize project

---
+++ android/app/src/main/kotlin/com/example/clean_ui_code_implementation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clean_ui_code_implementation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/product_detail/product_detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  const ProductDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
